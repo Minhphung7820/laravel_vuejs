59001eef2aa64d9375dfc6eee3bfab2b2da21776
Trương Minh Phung <minhphung485@gmail.com>
2024-11-09 17:02:42 +0700

    up

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,6 +4,7 @@ import About from '../components/About.vue';
 import ProductList from '../components/Product/ProductList.vue';
 import ProductForm from '../components/Product/ProductForm.vue';
 import ProductDetail from '../components/Product/ProductDetail.vue';
+import NotFound from '../components/NotFound.vue';
 
 const routes = [
   { path: '/', component: Home },
@@ -11,7 +12,9 @@ const routes = [
   { path: '/products', component: ProductList },
   { path: '/products/create', component: ProductForm },
   { path: '/products/:id', component: ProductDetail },
-  { path: '/products/:id/edit', component: ProductForm }
+  { path: '/products/:id/edit', component: ProductForm },
+  // Route 404 phải đặt ở cuối
+  { path: '/:pathMatch(.*)*', component: NotFound } // Bắt mọi đường dẫn không phù hợp
 ];
 
 const router = createRouter({
