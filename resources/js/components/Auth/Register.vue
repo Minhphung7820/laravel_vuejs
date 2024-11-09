5964773af849d9fb423a3b06a46a92239fcc477b
Trương Minh Phung <minhphung485@gmail.com>
2024-11-09 21:50:14 +0700

    fix

--- resources/js/components/Auth/Register.vue
+++ resources/js/components/Auth/Register.vue
@@ -47,7 +47,7 @@ export default {
         });
         this.$router.push('/login');
       } catch (error) {
-        this.errorMessage = 'Registration failed. Please try again.';
+        this.errorMessage = this.response.data.message;
       }
     },
   },
