a679fa8164b6586c1022d35e5be70dfcbf76e34d
Trương Minh Phung <minhphung485@gmail.com>
2024-11-10 10:44:50 +0700

    up

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios';
+import '../css/global.css';
 
 const baseURL =
   import.meta.env.VITE_APP_BASE_URL;
