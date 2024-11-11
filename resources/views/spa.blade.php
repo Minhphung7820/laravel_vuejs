4789ae09caa651796da6429831a27d1f154e5e47
Trương Minh Phung <minhphung485@gmail.com>
2024-11-11 21:14:48 +0700

    fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>App</title>
+  <title>App <?php echo env('APP_URL') ?></title>
   @vite('resources/css/app.css')
 </head>
 
